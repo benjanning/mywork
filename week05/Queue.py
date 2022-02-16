2343bf4c3753b99487f4a21897e46350b48e0a50
Ben Janning <bjdataanalytics@gmail.com>
2022-02-16 14:50:38 +0000

    week 05 work

---
+++ week05/Queue.py
@@ -0,0 +1,25 @@
+# a program that puts 10 random numbers into a queue(list), the
+# program should then output all the values in the queue, then take the
+# numbers from the queue one at a time, print it and the current numbers still
+# in the queue
+# Author: Ben Janning
+
+import random
+queue = []
+numberOfNumbers = 10
+rangeTo = 100
+
+for n in range (0, numberOfNumbers) :
+    queue.append(random.randint(0,rangeTo))
+
+print ("queue is {}".format(queue))
+
+while len(queue) !=0:
+
+    currentNumber = queue.pop(0)
+    print ("current Number is {} and the queue is {}".format(
+        currentNumber, queue))
+
+print ("the queue is now empty")
+
+
---
+++ week05/summermonths.py
@@ -0,0 +1,11 @@
+# Creating a Tuple that stores the months of the year
+# Creating a tuple from that Tuple with just the Summer months. 
+#Print out the summer months one at a time.
+# Author: Ben Janning
+
+months = ("January", "February", "March", "April", "May", "June", "July", 
+"August", "September", "October", "November", "December")
+
+summer = months [4:7]
+for month in summer:
+    print (month)
